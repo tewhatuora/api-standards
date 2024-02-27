e71a2ef3c3aca365bf972249b5a381571085fb52
Swithin Foote <swithin.foote@middleware.co.nz>
2024-02-27 21:26:41 +1300

    Going back to Draft for readability

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ module.exports = async function createConfig() {
             ],
             versions: {
               current: {
-                label: 'unpublished draft',
+                label: 'Draft',
                 path: 'draft',
               },
             },
